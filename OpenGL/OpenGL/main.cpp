c81962dfbaf1a572f00f36c6f5b428158d84f88d
NamiNaziri <naminaziri1@gmail.com>
2022-03-27 14:44:10 +0430

    integrated imgui to the project

--- OpenGL/OpenGL/main.cpp
+++ OpenGL/OpenGL/main.cpp
@@ -5,6 +5,10 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
 #include "Camera.h"
 #include "Shader.h"
 #include "STB/stb_image.h"
@@ -30,6 +34,7 @@ void WindowResizeHandler(GLFWwindow* window, int w, int h);
 // Functions
 
 GLFWwindow* CreateWindow();
+void SetupImgui(GLFWwindow* window);
 unsigned int loadTexture(const char* path);
 
 
@@ -59,10 +64,59 @@ bool ShowModel = true;
 
 #define POINT_LIGHTS_NUM 4
 
+
+// IMgui
+
+
 int main()
 {
 
 	GLFWwindow* window = CreateWindow();
+
+	//SetupImgui( window);
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+	//io.ConfigViewportsNoAutoMerge = true;
+	//io.ConfigViewportsNoTaskBarIcon = true;
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+	ImGuiStyle& style = ImGui::GetStyle();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+	}
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
+
+	// Load Fonts
+	// - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+	// - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+	// - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+	// - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+	// - Read 'docs/FONTS.md' for more instructions and details.
+	// - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+	//io.Fonts->AddFontDefault();
+	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+	//ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+	//IM_ASSERT(font != NULL);
+	
 
 	Shader SimpleShader("VertexShader.vert", "FragmentShader.frag");
 	Shader LightShader("SkeletonVertexShader.vert", "LightFragmentShader.frag");
@@ -175,7 +229,10 @@ int main()
 
 		glfwPollEvents();
 
+		
 
+
+		
 		// input
 		processInput(window);
 		glfwSetCursorPosCallback(window, mouse_callback);
@@ -194,6 +251,17 @@ int main()
 
 		if ((now - lastFrameTime) >= fpsLimit)
 		{
+
+			// Start the Dear ImGui frame
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+
+			ImGui::ShowDemoWindow();
+
+
+			ImGui::Render();
+			
 			// draw your frame here
 			// rendering commands
 
@@ -301,6 +369,25 @@ int main()
 			{
 				lights[i].Render(LightShader);
 			}
+
+
+			
+
+
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+			// Update and Render additional Platform Windows
+	   // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+	   //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+			if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+			{
+				GLFWwindow* backup_current_context = glfwGetCurrentContext();
+				ImGui::UpdatePlatformWindows();
+				ImGui::RenderPlatformWindowsDefault();
+				glfwMakeContextCurrent(backup_current_context);
+			}
+			
+			
 			// check and call events and swap buffers
 			glfwSwapBuffers(window);
 
@@ -316,9 +403,14 @@ int main()
 
 
 	}
+	// Cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
+	glfwDestroyWindow(window);
 	glfwTerminate();
-
+	
 	return 0;
 }
 
@@ -409,7 +501,7 @@ GLFWwindow* CreateWindow()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
-	GLFWwindow* window = glfwCreateWindow(Width, Height, "OpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(Width, Height, "Skeletal Animation", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW Window" << std::endl;
@@ -417,7 +509,8 @@ GLFWwindow* CreateWindow()
 		//return -1;
 	}
 	glfwMakeContextCurrent(window);
-
+	glfwSwapInterval(1); // Enable vsync
+	
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -433,6 +526,10 @@ GLFWwindow* CreateWindow()
 	return window;
 }
 
+void SetupImgui(GLFWwindow* window)
+{
+	
+}
 
 void WindowResizeHandler(GLFWwindow* window, int w, int h)
 {
