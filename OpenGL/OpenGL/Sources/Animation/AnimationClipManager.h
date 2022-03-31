86e623001767d05c379eabcd6822e98d3cedc1b2
NamiNaziri <naminaziri1@gmail.com>
2022-03-31 14:25:40 +0430

    Changed file structure

--- OpenGL/OpenGL/Sources/Animation/AnimationClipManager.h
+++ OpenGL/OpenGL/Sources/Animation/AnimationClipManager.h
@@ -13,7 +13,7 @@ public:
 private:
 
 	Skeleton* skeleton;
-	std::vector<AnimationClip* > loadedAnimationClips; //TODO change to map of string(name of the clip) and clip;
+	std::vector<AnimationClip*> loadedAnimationClips; //TODO change to map of string(name of the clip) and clip;
 	void LoadAnimationClips(const aiScene* scene);
 	AnimationKeyframes ProcessAnimationChannel(aiNodeAnim* channel);
 
--- OpenGL/OpenGL/Sources/main.cpp
+++ OpenGL/OpenGL/Sources/main.cpp
@@ -238,7 +238,6 @@ int main()
 	{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
 		const double now = glfwGetTime();
 		deltaTime = now - lastUpdateTime;
 		// input
